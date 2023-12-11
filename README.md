758318badf3b84844a0c9ccf57635e7e7b112ef4
yunanjeong <njhgle@gmail.com>
2023-12-11 19:29:08 +0900

    s3 upload 작업 중

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ftp-to-s3
+
+awscli가 boto3보다 빠르다. 용량이 클땐 awscli가 유리하다.
+ncftpget은 recursive 다운로드가 되지만, ftplib은 직접 구현해야 한다.
--- main.py
+++ main.py
@@ -50,6 +50,32 @@ def download_from_ftp(target_dir):
         # alert
 
 
+# def upload_to_s3(targetdate, tmp_path, upload_info):
+#     """s3로 업로드.
+
+#     Args:
+#         targetdate (datetime): 작업대상날짜
+#         tmp_path (str): 저장 경로 (config)
+#         upload_info (dict): s3_bucket, s3_path, options (config)
+
+#     """
+#     # logger.debug(">>> Uploading files to AWS S3 ...")
+#     s3_bucket, s3_path, options = upload_info.values()
+#     date_dict = get_date_dict(targetdate)
+#     s3_path = s3_path.format(**date_dict)  # 날짜 템플릿과 매치되는 부분에 삽입
+
+#     # aws = get_awscli_path()
+#     aws = 'aws' if 'yunantest' in s3_bucket else '/usr/local/bin/aws'
+
+#     # s3로 csv파일 업로드
+#     cmd = f'{aws} s3 cp {tmp_path}/ s3://{s3_bucket}/{s3_path} {options}' # NOQA
+#     os.system(cmd)
+
+#     # 로컬에 남은 파일 삭제
+#     # logger.debug(">>> Removing temporary files in Local Drive...")
+#     os.system(f'rm -rf {tmp_path}')
+
+
 yesterday = get_yesterday("%Y-%m/%d")
 servers = get_servers()
 for server in servers:
@@ -57,24 +83,9 @@ for server in servers:
     download_from_ftp(target_dir)
 
 
-# # S3 클라이언트 생성
-# s3 = boto3.client('s3', aws_access_key_id='access_key', aws_secret_access_key='secret_key')  # env
-# def upload_to_s3(file):
-#     # S3 버킷에 파일 업로드
-#     with open(file, 'rb') as data:
-#         s3.upload_fileobj(data, 'bucket_name', file)  # env
-
-
-
-# def sync_ftp_to_s3():
-#     # FTP 서버의 모든 파일을 S3로 복사
-#     files = ftp.nlst()   # nlst(): 특정 디렉토리에 있는 요소들의 이름을 리스트로 반환
-#     for file in files:
-#         local_file = download_from_ftp(file)
-#         upload_to_s3(local_file)
-#         os.remove(local_file)  # 로컬에 저장된 파일 삭제
-
+# TODO: sleep으로 하면 시간 밀릴 수 있음. 특정시간에 실행해야함. crontab 쓰던지 현재시간 기반으로 해야함.
 # 매일 한번씩 실행
 # while True:
 #     sync_ftp_to_s3()
 #     time.sleep(86400)  # 24시간 대기
+
